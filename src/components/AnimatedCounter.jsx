d411aebb734703436243fdb1a672b61ea95ff825
Ashish <ashishd595@gmail.com>
2025-05-28 13:47:32 +0530

    Update max-width:1024px for desktop view

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import CountUp from 'react-countup';
 
+
 const AnimatedCounter = ({
   icon,
   label,
